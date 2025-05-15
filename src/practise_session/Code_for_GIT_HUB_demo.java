9571aafa760b4608cf65491928db07e6f56f47e9
uday <udaykumarsappidi@gmail.com>
2025-05-15 16:40:21 +0530

    Data is added by user 2 in class

--- src/practise_session/Code_for_GIT_HUB_demo.java
+++ src/practise_session/Code_for_GIT_HUB_demo.java
@@ -4,6 +4,11 @@ public class Code_for_GIT_HUB_demo {
 
 	public static void main(String[] args) {
 		System.out.println("Data added By User 1");
+		System.out.println("Data added By USer 2");
+		System.out.println("Data added By USer 2");
+		System.out.println("Data added By USer 2");
+		System.out.println("Data added By USer 2");
+		System.out.println("Data added By USer 2");
 
 	}
 
